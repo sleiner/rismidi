c77a1c98719257a41ef262ba57931779afae4f58
Simon Leiner <simon@leiner.me>
2022-10-16 16:31:16 +0200

    Group MIDI-related functionality in dedicated module

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 // #![warn(missing_docs)] FIXME
 
-mod constants;
 mod error;
 mod has_channel;
+mod midi;
 pub mod param;
 
-pub use constants::*;
 pub use error::RismidiError;
 pub use has_channel::HasChannel;
-pub use param::{MidiChannel, OptionalMidiChannelParam};
+pub use midi::{constants::*, MidiChannel};
+pub use param::OptionalMidiChannelParam;
---
+++ src/midi/mod.rs
@@ -0,0 +1,4 @@
+pub mod constants;
+pub mod midi_channel;
+
+pub use midi_channel::MidiChannel;
